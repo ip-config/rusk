bd44abd1d0b525d9f1a0365e25c3ed3bfb7405e0
Jules de Smit <julesdesmit@gmail.com>
2020-04-28 16:19:10 +0200

    Add client method for creating a bid

--- src/client.rs
+++ src/client.rs
@@ -9,6 +9,7 @@ pub async fn create_transaction(
     value: u64,
     fee: u64,
     recipient: PublicKey,
+    obfuscated: bool,
 ) -> Result<rpc::Transaction, Box<dyn std::error::Error>> {
     let mut client = client().await?;
     let request = tonic::Request::new(sk.view_key().into());
@@ -20,10 +21,38 @@ pub async fn create_transaction(
         recipient: Some(recipient),
         value,
         fee,
-        obfuscated: false,
+        obfuscated,
     });
     let response = client.new_transaction(request).await?;
     Ok(response.into_inner())
+}
+
+pub async fn create_bid(
+    sk: SecretKey,
+    value: u64,
+    fee: u64,
+) -> Result<rpc::ContractCall, Box<dyn std::error::Error>> {
+    let mut client = client().await?;
+    let request = tonic::Request::new(sk.view_key().into());
+    let response = client.full_scan_owned_notes(request).await?;
+
+    let request = tonic::Request::new(rpc::BidTransactionRequest {
+        m: [0u8; 32].to_vec(),
+        tx: Some(rpc::NewTransactionRequest {
+            sk: Some(sk.into()),
+            inputs: response.into_inner().notes,
+            recipient: Some(sk.public_key().into()),
+            value,
+            fee,
+            obfuscated: true,
+        }),
+    });
+    let response = client.new_bid(request).await?;
+    Ok(rpc::ContractCall {
+        contract_call: Some(rpc::contract_call::ContractCall::Bid(
+            response.into_inner(),
+        )),
+    })
 }
 
 pub async fn validate_state_transition(
